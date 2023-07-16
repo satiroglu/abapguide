94ba9b31bb44658954eaf4b4330dcdbdff592514
Ahmet Erturk <aesatiroglu@gmail.com>
2023-07-16 04:15:08 +0300

    category and post model added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+# Category Actions
+@admin.action(description="Seçili kategorileri yayımla")
+def make_category_published(modeladmin, request, queryset):
+    queryset.update(status='1')
+
+
+@admin.action(description="Seçili kategorileri taslak yap")
+def make_category_draft(modeladmin, request, queryset):
+    queryset.update(status='0')
+
+
+# Category Admin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'createdOn', 'updatedOn', 'status')
+    list_display_links = ['id', 'name']
+    list_filter = ['status']
+    ordering = ['id']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    # list_editable = ['status']
+    list_per_page = 20
+    # special actions
+    actions = [make_category_published, make_category_draft]
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+# Posts Actions
+@admin.action(description="Seçili yazıları yayınla")
+def make_post_published(modeladmin, request, queryset):
+    queryset.update(status='1')
+
+
+@admin.action(description="Seçili yazıları taslak yap")
+def make_post_draft(modeladmin, request, queryset):
+    queryset.update(status='0')
+
+
+# Posts Admin
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'author', 'createdOn', 'status')
+    list_filter = ('status','author')
+    list_display_links = ['title']
+    ordering = ['id']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    # list_editable = ['status']
+    list_per_page = 20
+    # special actions
+    actions = [make_post_published, make_post_draft]
+
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = (
+    (0, "Taslak"),
+    (1, "Yayında")
+)
+
+
+# Category Model
+class Category(models.Model):
+    # Category
+    name = models.CharField(verbose_name="Kategori Adı", max_length=200, unique=True, )
+    slug = models.SlugField(verbose_name="Slug", max_length=200, unique=True)
+    description = models.TextField(verbose_name="Kısa Açıklama", blank=True, max_length=200,
+                                   help_text="Maksimum 200 karakter")
+
+    # SEO
+    seoTitle = models.CharField(verbose_name="SEO Başlığı", max_length=70, blank=True,
+                                help_text="Maksimum 70 karakter")
+    seoKeywords = models.CharField(verbose_name="Anahtar Kelimeler", blank=True, max_length=300,
+                                   help_text="Anahtar kelimeleri virgül ile ayırın")
+    seoDescription = models.CharField(verbose_name="SEO Açıklama", blank=True, max_length=150,
+                                      help_text="Maksimum 160 karakter")
+
+    # Other
+    createdOn = models.DateTimeField(verbose_name="Oluşturulma Tarihi", auto_now_add=True)
+    updatedOn = models.DateTimeField(verbose_name="Güncellenme Tarihi", auto_now=True)
+    status = models.IntegerField(verbose_name="Durum", choices=STATUS, default=1)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Yazar", )
+
+    class Meta:
+        ordering = ['-createdOn']
+        verbose_name = 'Kategori'
+        verbose_name_plural = 'Kategoriler'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Post(models.Model):
+    # Post
+    title = models.CharField(verbose_name="Yazı Başlığı", max_length=200, unique=True)
+    slug = models.SlugField(verbose_name="Slug", max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Kategori")
+    content = models.TextField(verbose_name="Yazı")
+
+    # SEO
+    seoTitle = models.CharField(verbose_name="SEO Başlığı", max_length=70, blank=True,
+                                help_text="Maksimum 70 karakter")
+    seoKeywords = models.CharField(verbose_name="Anahtar Kelimeler", blank=True, max_length=300,
+                                   help_text="Anahtar kelimeleri virgül ile ayırın")
+    seoDescription = models.CharField(verbose_name="SEO Açıklama", blank=True, max_length=150,
+                                      help_text="Maksimum 160 karakter")
+
+    # Other
+    createdOn = models.DateTimeField(verbose_name="Oluşturulma Tarihi", auto_now_add=True)
+    updatedOn = models.DateTimeField(verbose_name="Güncellenme Tarihi", auto_now=True)
+    status = models.IntegerField(verbose_name="Durum", choices=STATUS, default=1)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts', verbose_name="Yazar",)
+
+    class Meta:
+        ordering = ['-createdOn']
+        verbose_name = 'Yazı'
+        verbose_name_plural = 'Yazılar'
+
+    def __str__(self):
+        return self.title
